70ad2a3f72f1413b407e51b54d0202f8f3a720a5
Necati Alperen Işık <necatialperen2@gmail.com>
2024-08-02 11:21:45 +0300

    Initial Commit

---
+++ ios-TextDetectionSample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ios-TextDetectionSample
+//
+//  Created by Necati Alperen IŞIK on 2.08.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ios-TextDetectionSampleUITests/ios_TextDetectionSampleUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  ios_TextDetectionSampleUITestsLaunchTests.swift
+//  ios-TextDetectionSampleUITests
+//
+//  Created by Necati Alperen IŞIK on 2.08.2024.
+//
+
+import XCTest
+
+final class ios_TextDetectionSampleUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
